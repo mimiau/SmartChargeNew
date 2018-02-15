be0af488cae9c43cdbe0f4d049b30a41b66fa726
Mateusz <matweasley@gmail.com>
2018-02-15 17:37:57 +0100

    Added Reduced Local Search (RLS1) algorithm

--- src/main/java/SmartCharge.java
+++ src/main/java/SmartCharge.java
@@ -10,9 +10,7 @@
 
 import org.pcj.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -174,6 +172,8 @@ public class SmartCharge implements StartPoint {
 
         float[][] distancesArray = new float[LCluster.size()][LStation.size()];  //[i][j]
         distancesArray = AuxTools.makeDistanceArray(LCluster, LStation);
+        int[][] sortedDistancesArrayIndexesArray = new int[distancesArray.length][distancesArray[0].length];
+        sortedDistancesArrayIndexesArray = AuxTools.makeSortedDistanceArrayIndexesArray(distancesArray);
 
         int searchRangeStartIndex = AuxTools.findSearchRangeStartIndex(LStation);
         int searchRangeStopIndex = LStation.size()-1;
@@ -214,7 +214,9 @@ public class SmartCharge implements StartPoint {
         //initialize(boolean[] localSolution, List<station> LStation, int thisManyStations, int alreadyAddedStations, int searchRangeStartIndex,  int searchRangeStopIndex)
 
 
-
+        for (int i = searchRangeStartIndex+200; i < searchRangeStartIndex+thisManyStations-200; i++) {
+            localSolution[i] = true;
+        }
 
 
         //TU WKLEJ FUNKCJĘ - STOP
@@ -239,27 +241,163 @@ public class SmartCharge implements StartPoint {
 
 
 
-
-
-
-
-
-
-
-//        //RANDOM INITIALIZATION (B option)
-//        for (int i = searchRangeStartIndex; i < searchRangeStartIndex+thisManyStations; i++) {
-//            localSolution[i] = true;
-//        }
-
-
-        int[][] sortedDistancesArrayIndexesArray = AuxTools.makeSortedDistanceArrayIndexesArray(distancesArray);
-
+        //OBJ FUNCT TEMP
         float objFunctVal = AuxTools.objectiveFunction3(localSolution, distancesArray, sortedDistancesArrayIndexesArray);
         System.out.println("Objective function value = " + objFunctVal);
+        //OBJ FUNCT TEMP
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //REDUCED LOCAL SEARCH (RLS1) - part of VARIABLE NEIGHBORHOOD SEARCH (VNS) - START
+        //ls0: creating S as TempLocal Solution, creating S' as tempTempLocalSolution
+        boolean tempLocalSolution[] = new boolean[localSolution.length]; //RLS1 notation: S ; VNS notation S'
+        boolean tempTempLocalSolution[] = new boolean[localSolution.length]; //RLS1 notation: S' ; VNS notation S"
+        System.arraycopy(localSolution, 0, tempLocalSolution, 0, localSolution.length);
+        System.arraycopy(localSolution, 0, tempTempLocalSolution, 0, localSolution.length);
+
+        System.out.println("LS-START ");
+
+        //ls1:
+        boolean improve = true; // substituted with 2 statements below (both)
+        float improveValue = Float.MAX_VALUE;
+        float startingValue = AuxTools.objectiveFunction3(tempLocalSolution, distancesArray, sortedDistancesArrayIndexesArray);
+
+        System.out.println("While(improve)-START");
+        //ls2:
+        int improveWhileCounter = 0;
+        //while (improve) { //instead (see below) we consider the improvement value relative to the value at the beginning
+        while (improveValue/startingValue > 0.0001) { //0.0001
+            improveWhileCounter ++;
+            System.out.println("While(improve) " + improveWhileCounter);
+
+            //ls3:
+            improve = false; //substituted with value below
+            improveValue = 0;
+
+            //ls4: instead of measuring changes of the functions (deltaFDrop) we calculate the functions themselves (Fdrop)
+            //     and compare them directly.
+            float fDrop = Float.MAX_VALUE;
+            int jDrop = -1; //there is "IF" below so there is a warning about potencial lack of initialization. The
+            // value assigned here does not mean anything. Meaningful value is assigned at ls8.
+
+            float f;
+
+            //ls5: -we exclude real world stations (they are immovable) hence searchRangeStartIndex (they are placed at
+            //      the beginning of solution array).
+            //     -solution array is longer than the length of an actual solution. There is no need to iterate through
+            //      it all hence searchRangeStopIndex is applied.
+            System.out.println("SEARCH FOR jDROP -START");
+            for (int j = searchRangeStartIndex; j <= searchRangeStopIndex; j++) {
+                if (tempLocalSolution[j] == true) {// has to be equal to true because we search through active stations only
+                    //ls6: f represents value of objective function increased as a result of substracting one station (of
+                    //     index: j) from the solution.
+                    tempLocalSolution[j] = false;
+                    f = AuxTools.objectiveFunction3(tempLocalSolution, distancesArray, sortedDistancesArrayIndexesArray);
+                    tempLocalSolution[j] = true;
+
+                    //ls7: we want to drop the station whoose dissapearance increased objective function the less.
+                    if (f < fDrop) {
+                        //ls8:
+                        jDrop = j;
+                        //ls9:
+                        fDrop = f;
+                    }
+                }
+            } // here we end search for the function to drop. It is indicated by jDrop index of tempLocalSoluton.
+
+            //ls10:
+            tempTempLocalSolution[jDrop] = false;
+
+            //ls11: we assign fAdd max value as we search for the smallest f value not biggest (see ls14)
+            float fAdd = Float.MAX_VALUE;
+            int jAdd = -1; // there is "IF" below so there is a warning about potencial lack of initialization. The
+            // value assigned here does not mean anything. Meaningful value is assigned at ls15.
+            System.out.println("SEARCH FOR jADD -START");
+            //ls12:
+            for (int j = searchRangeStartIndex; j <= searchRangeStopIndex; j++) {
+                if (tempLocalSolution[j] == false) {// has to be equal to true because we search through active stations
+                                                    // only.
+
+                    //ls13:
+                    tempTempLocalSolution[j] = true;
+                    f = AuxTools.objectiveFunction3(tempTempLocalSolution, distancesArray, sortedDistancesArrayIndexesArray);
+                    tempTempLocalSolution[j] = false;
+
+                    //ls14: we want to minimize objective function so we search for the smallest f and assign it to fAdd
+                    if (f < fAdd) {
+                        //ls15:
+                        jAdd = j;
+                        //ls16:
+                        fAdd = f;
+                    }
+                }
+            }
+
+            //ls17:
+            tempTempLocalSolution[jAdd] = true;
+
+            //ls18: we want to minimize objective function so we substitute S' (RLS1 notation) for S (RLS1 notation) when
+            //      S' (RLS1 notation) is smaller.
+            float fNew = AuxTools.objectiveFunction3(tempTempLocalSolution, distancesArray, sortedDistancesArrayIndexesArray);
+            float fOld = AuxTools.objectiveFunction3(tempLocalSolution, distancesArray, sortedDistancesArrayIndexesArray);
+            if (fNew < fOld) {
+                //above: if( f(S') < f(S) ) where S' = S - {jDrop} + {jAdd}
+
+                //ls19:
+                tempLocalSolution[jDrop] = false;
+                tempLocalSolution[jAdd] = true;
+
+                //ls20:
+                improve = true; //this is substituted with value of improvement
+                improveValue = fOld - fNew;
+
+
+                //lsAdditional: printing every 5th solution to file
+                if(improveWhileCounter % 5 == 0){
+
+                    String fileName = "/Users/Mateusz/Desktop/temp_" + improveWhileCounter + ".txt";
+
+                    PrintWriter paper2 = new PrintWriter(fileName, "UTF-8");
+                    for (int i = 0; i < searchRangeStopIndex; i++) {
+                        if(tempLocalSolution[i] == true){
+                            paper2.println(LStation.get(i).getX() + ";" + LStation.get(i).getY());
+                        }
+                    }
+                    paper2.close();
+                }
+
+                //lsAdditional: adding this solution to file
+                try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("/Users/Mateusz/Desktop/ObjFunct", true)))) {
+                    out.println(fNew);
+                }catch (IOException e) {
+                    System.err.println(e);
+                }
+
+            }
+        }
+        //REDUCED LOCAL SEARCH (RLS1) - part of VARIABLE NEIGHBORHOOD SEARCH (VNS) - END
 
 
 
